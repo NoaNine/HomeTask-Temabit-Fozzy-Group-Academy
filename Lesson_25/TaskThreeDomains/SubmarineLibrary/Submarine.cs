3cf392acea70c13779a2e4b558b647ecce563131
Noa <65802927+NoaNine@users.noreply.github.com>
2021-12-18 17:35:22 +0200

    Add files via upload

---
+++ Lesson_25/TaskThreeDomains/SubmarineLibrary/Submarine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubmarineLibrary
+{
+    public class Submarine : MarshalByRefObject
+    {
+        public void Attack()
+        {
+            Console.WriteLine("Let out a torpedo");
+        }
+    }
+}
---
+++ Lesson_25/TaskThreeDomains/TankLibrary/Tank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankLibrary
+{
+    public class Tank : MarshalByRefObject
+    {
+        public void Attack()
+        {
+            Console.WriteLine("Shot");
+        }
+    }
+}
